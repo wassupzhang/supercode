1245823324b407484c420d9fdacdeb7557a8d478
wassupzhang <91063869+wassupzhang@users.noreply.github.com>
2021-10-01 19:38:25 +0800

    Add files via upload

--- for.c
+++ for.c
@@ -2,16 +2,16 @@
 
 #include<stdio.h>//nµÄ½×³Ë
 
-int main()
-{
-	int i = 0;
-	int n = 0;
-	int ret = 1;
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
-	{
-		ret = ret * i;
-	}
-	printf("ret=%d\n", ret);
-	return 0;
-}
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("ret=%d\n", ret);
+//	return 0;
+//}
