c85569530d89a07cf11ef63c3767d01cd6b7c114
wassupzhang <91063869+wassupzhang@users.noreply.github.com>
2021-11-02 20:44:40 +0800

    Add files via upload

--- game.c
+++ game.c
@@ -1,147 +1,101 @@
-#define  _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include "game.h"
 
-void InitBoard(char board[ROW][COL], int row, int col)
+void InitBoard(char board[ROWS][COLS], int rows, int cols,char set)
 {
 	int i = 0;
 	int j = 0;
-	for (i = 0; i < row; i++)
+	for (i = 0; i < rows; i++)
 	{
-		for (j = 0; j < col; j++)
+		for (j = 0; j < cols; j++)
 		{
-			board[i][j] = ' ';
+			board[i][j] = set;
 		}
 	}
 }
 
-void DisplayBoard(char board[ROW][COL], int row, int col)
+void DisplayBoard(char board[ROWS][COLS], int row, int col)
 {
 	int i = 0;
-	for (i = 0; i < row; i++)
+	int j = 0;
+	//打印列号
+	for (i = 0; i <= col; i++)
 	{
-		//1.打印一行数据
-		int j = 0;
-		for (j = 0; j < col; j++)
+		printf("%d ", i);
+	}
+	printf("\n");
+	for (i = 1; i <= row; i++)
+	{
+		printf("%d ", i);
+		for (j = 1; j <= col; j++)
 		{
-			printf(" %c ", board[i][j]);
-			if (j < col - 1)
-				printf("|");
+			printf("%c ", board[i][j]);
 		}
 		printf("\n");
-
-		//printf(" %c | %c | %c \n",board[i][0],board[i][1],board[i][2]);上面的为优化后的
-
-		//2.打印分割行
-
-		//if(i<row-1)
-		//printf("---|---|---\n");下面为优化后的
-
-		if (i < row - 1)
-		{
-			for (j = 0; j < col; j++)
-			{
-				printf("---");
-				if(j<col-1)
-				printf("|");
-			}
-			printf("\n");
-		}
 	}
 }
 
-void PlayerMove(char board[ROW][COL], int row, int col)
+void SetMine(char board[ROWS][COLS], int row, int col)
+{
+	int count = EASY_COUNT;
+	while (count)
+	{
+		int x = rand() % row + 1;
+		int y = rand() % col + 1;
+		if (board[x][y] == '0')
+		{
+			board[x][y] = '1';
+			count--;
+		}
+	}
+}
+//字符 - 字符0变成数字，之后进行相加减
+int get_mine_count(char mine[ROWS][COLS],int x,int y)
+{
+	return mine[x - 1][y] +
+		mine[x - 1][y - 1] +
+		mine[x][y - 1] +
+		mine[x + 1][y - 1] +
+		mine[x + 1][y] +
+		mine[x + 1][y + 1] +
+		mine[x][y + 1] +
+		mine[x - 1][y + 1] - 8 * '0';
+}
+void FindMine(char mine[ROWS][COLS], char show[ROW][COLS], int row, int col)
 {
 	int x = 0;
 	int y = 0;
-	printf("玩家走：>\n");
-	printf("请输入要下的坐标：");
-	scanf("%d%d", &x, &y);
-	//判断x，y坐标的合法性
-	while (1)
-	{
+	int win = 0;
+	while (win < row*col - EASY_COUNT)
+    {
+		printf("请输入排查雷的坐标：");
+		scanf("%d%d", &x, &y);
 		if (x >= 1 && x <= row && y >= 1 && y <= col)
 		{
-			if (board[x - 1][y - 1] == ' ')
+			//坐标合法
+			//1.踩雷
+			if (mine[x][y] == '1')
 			{
-				board[x - 1][y - 1] = '*';
+				printf("很遗憾，你被炸死\n");
+				DisplayBoard(mine, row, col);
 				break;
 			}
-			else
+			else//不是雷
 			{
-				printf("该坐标被占用\n");
+					//计算x,y坐标周围有几个雷
+					int count = get_mine_count(mine, x, y);
+					show[x][y] = count + '0';
+					DisplayBoard(show, ROW, COL);
+					win++;
 			}
 		}
 		else
 		{
-			printf("坐标非法，请重新输入！\n");
-			break;
+			printf("输入坐标非法，请重新输入\n");
 		}
+    }
+	if (win == row * col - EASY_COUNT)
+	{
+	   printf("恭喜你，排雷成功\n");
 	}
 }
-void ComputerMove(char board[ROW][COL], int row, int col)
-{
-	int x = 0;
-	int y = 0;
-	printf("电脑走：>\n");
-	while (1)
-	{
-		x = rand() % row;
-		y = rand() % col;
-		if (board[x][y] == ' ')
-		{
-			board[x][y] = '#';
-			break;
-		}
-	}
-
-}
-
-int IsFull(char board[ROW][COL], int row, int col)
-{
-	int i = 0;
-	int j = 0;
-	for (i = 0; j < col; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			if (board[i][j] == ' ')
-			{
-				return 0;//没满
-			}
-		}
-	}
-	return 1;//满了
-}
-
-char IsWin(char board[ROW][COL], int row, int col)
-{
-	int i = 0;
-	//横三行
-	for (i = 0; i < row; i++)
-	{
-		if (board[i][0] == board[i][1] && board[i][2] && board[i][1] != ' ')
-		{
-			return board[i][1];
-		}
-	}
-	//竖三列
-	for (i = 0; i < col; i++)
-	{
-		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != ' ')
-		{
-			return board[1][i];
-		}
-	}
-	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
-		return board[1][1];
-	if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][i] != ' ')
-		return board[1][1];
-	//判断是否平局
-	if (1 == IsFull(board, ROW, COL))
-	{
-		return 'Q';
-
-	}
-	return 'C';
-}
-
